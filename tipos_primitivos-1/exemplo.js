8c57580f08bbb7e3eb40cfa0687a0c3dd0605102
devopsnot <eduardobsn.exe@gmail.com>
2023-04-04 18:40:06 -0300

    inicio

---
+++ tipos_primitivos-1/exemplo.js
@@ -0,0 +1,26 @@
+// Passando variaveis com soma;
+const soma = 10 + 10;
+const multiplicacao = 10 * 10;
+const operacao = (soma + multiplicacao) + 22;
+
+//console.log(operacao);
+
+///////////////////////////////////////////////////////////////
+
+// Exemplo para teste 01
+
+const nomeCompleto = "Eduardo" + "Stupp";
+const idade = 10 + 12;
+const full = (nomeCompleto + idade) + "Idade";
+
+console.log(full);
+
+// Exemplo para teste 02
+
+const minhaIdade = 22;
+const idadeLucas = 21;
+const idadeSamuel = 20;
+
+const idadeTodos = (minhaIdade + idadeLucas + idadeSamuel);
+
+console.log(idadeTodos);
